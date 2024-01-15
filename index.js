9a7bf1e8f6e20fb7b12325f5de31a3175d1f7b1a
Fr4nkD3v <frank.code31@gmail.com>
2024-01-14 20:56:58 -0500

    chore: exercise 3.7 - phonebook backend

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
@@ -26,6 +27,7 @@ let persons = [
 ];
 
 app.use(express.json());
+app.use(morgan('tiny'));
 
 app.get('/info', (_, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people<br/>${new Date()}</p>`)
